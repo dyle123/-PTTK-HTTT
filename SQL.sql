71a69ebdf8cfb79ea786fee5ee6e88d5f8a95a6b
Vy <lnyvy22@clc.fitus.edu.vn>
2025-03-15 11:10:48 +0700

    Update SQL-vy.sql

--- SQL.sql
+++ SQL.sql
@@ -1,5 +1,44 @@
 CREATE DATABASE PTTK
 GO
 USE PTTK
-GO
+ GO
+
+CREATE TABLE NhanVien
+MaNhanVien int (Primary Key),
+HoTen nvarchar(50),
+SoDienThoai char(10),
+Luong varchar(15),
+BoPhan nvarchar(20)
+
+CREATE TABLE PhieuThanhToan
+MaPhieuThanhToan int (Primary Key),
+SoTien int,
+PhanTramGiamGia int,
+TrangThaiThanhToan nvarchar(20),
+HinhThucThanhToan nvarchar(20),
+MaPhieuDangKy int
+
+CREATE TABLE HoaDonThanhToan
+MaHoaDon int (Primary Key),
+TongTien int,
+NgayThanhToan datetime,
+MaPhieuThanhToan int
+
+
+
+// Khóa ngoại ở đây
+ALTER TABLE PhieuThanhToan 
+ADD CONSTRAINT FK_PhieuThanhToan_PhieuDangKy FOREIGN KEY (MaPhieuDangKy) REFERENCES PhieuDangKy(MaPhieuDangKy);
+
+ALTER TABLE HoaDonThanhToan
+ADD CONSTRAINT FK_HoaDonThanhToan_PhieuThanhToan FOREIGN KEY (MaPhieuThanhToan) REFERENCES PhieuThanhToan(MaPhieuThanhToan)
+
+
+
+
+
+
+
+
+
 
