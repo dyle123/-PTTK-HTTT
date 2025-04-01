9fb2b5b8c48401e912d708452a8e503c6ef6bff9
DungLuongHoang <dungluonghoang312@gmail.com>
2025-04-01 15:43:28 +0700

    Pull nhớ chỉnh tại tui thêm cái kết nối sql của tui dô á

--- server.js
+++ server.js
@@ -39,14 +39,29 @@ app.use(session({
 //     },
 // };
 
+// // Cấu hình kết nối SQL Server
+// const config = {
+//     // server: '127.0.0.1', // Địa chỉ IP của máy chủ SQL Server
+//     server: '192.168.174.1',
+//     port: 1433, // Cổng SQL Server
+//     database: 'PTTK',
+//     user: 'BENU',
+//     password: 'benu123',
+//     options: {
+//         encrypt: false, // Không cần mã hóa
+//         enableArithAbort: true, // Bật xử lý lỗi số học
+//         connectTimeout: 30000, // Thời gian chờ 30 giây
+//     },
+// };
+
 // Cấu hình kết nối SQL Server
 const config = {
     // server: '127.0.0.1', // Địa chỉ IP của máy chủ SQL Server
-    server: '192.168.174.1',
+    server: '192.168.1.11',
     port: 1433, // Cổng SQL Server
     database: 'PTTK',
-    user: 'BENU',
-    password: 'benu123',
+    user: 'dungluonghoang',
+    password: 'teuklee1983#',
     options: {
         encrypt: false, // Không cần mã hóa
         enableArithAbort: true, // Bật xử lý lỗi số học
