1044f284c390f89348d2e9b3ffe6709ab2c35a1b
Vy <lnyvy22@clc.fitus.edu.vn>
2025-05-01 00:55:26 +0700

    ê t thử làm nút thêm thí sinh á, m có làm rồi thì hú  t backup lại bản cũ cho nhenhe

--- server.js
+++ server.js
@@ -31,40 +31,40 @@ app.use(session({
 
 
 
-const config = {
-    server: '192.168.102.1', // Địa chỉ IP của máy chủ SQL Server
-    port: 1433, // Cổng SQL Server
-    database: 'PTTK',
-    user: 'sa',
-    password: '1928374650Vy',
-    options: {
-        encrypt: false, // Không cần mã hóa
-        enableArithAbort: true, // Bật xử lý lỗi số học
-        connectTimeout: 30000, // Thời gian chờ 30 giây
-    },
-};
-async function sqlQuery(query, params = {}) {
-    try {
-        const pool = await sql.connect({
-            user: 'sa',
-            password: '12345678',
-            database: 'PTTK',
-            server: 'localhost',
-            options: { encrypt: false, trustServerCertificate: true }
-        });
+// const config = {
+//     server: '192.168.102.1', // Địa chỉ IP của máy chủ SQL Server
+//     port: 1433, // Cổng SQL Server
+//     database: 'PTTK',
+//     user: 'sa',
+//     password: '1928374650Vy',
+//     options: {
+//         encrypt: false, // Không cần mã hóa
+//         enableArithAbort: true, // Bật xử lý lỗi số học
+//         connectTimeout: 30000, // Thời gian chờ 30 giây
+//     },
+// };
+// async function sqlQuery(query, params = {}) {
+//     try {
+//         const pool = await sql.connect({
+//             user: 'sa',
+//             password: '12345678',
+//             database: 'PTTK',
+//             server: 'localhost',
+//             options: { encrypt: false, trustServerCertificate: true }
+//         });
 
-        const request = pool.request();
-        for (const param in params) {
-            request.input(param, params[param]);
-        }
+//         const request = pool.request();
+//         for (const param in params) {
+//             request.input(param, params[param]);
+//         }
 
-        const result = await request.query(query);
-        return result.recordset;
-    } catch (error) {
-        console.error("❌ Lỗi SQL:", error);
-        throw error;
-    }
-}
+//         const result = await request.query(query);
+//         return result.recordset;
+//     } catch (error) {
+//         console.error("❌ Lỗi SQL:", error);
+//         throw error;
+//     }
+// }
 
 
 
